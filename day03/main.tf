d2c75b1fd4576239656141abd8889bb006553f8d
shafdo <yugantha1468@gmail.com>
2026-03-05 22:54:47 +0530

    first commit

---
+++ day03/main.tf
@@ -0,0 +1,38 @@
+# TF version
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.35.0"
+    }
+  }
+}
+
+# Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+# Resource: VPC
+resource "aws_vpc" "day30_tf_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Day 30 TF VPC"
+    Environment = "Dev"
+  }
+}
+
+
+# Resource: S3
+resource "aws_s3_bucket" "example" {
+  bucket = "day30-tf-bucket"
+
+  tags = {
+    Name        = "day30-tf-bucket"
+    Environment = "Dev"
+    
+    # Implicit dependency
+    VPCId       = aws_vpc.day30_tf_vpc.id
+  }
+}
